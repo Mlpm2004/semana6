296af67f27e2100dd4240e6ffb95aad667aa8987
Mlpm2004 <94148203+Mlpm2004@users.noreply.github.com>
2021-11-24 09:07:07 -0300

    Exercício 4

--- Sem6Listaex4.js
+++ Sem6Listaex4.js
@@ -1,7 +1,7 @@
-/* function fullName(nome='Jane', sobrenome='Doe') {
+/* function fullName(nome='Jane', sobrenome='Doe') {  FUNçÃO FATORADA
   return nome+" "+sobrenome;
 }  */
-const fullName = (nome='Jane', sobrenome='Doe')=> nome+" "+sobrenome
+const fullName = (nome='Jane', sobrenome='Doe')=> nome+" "+sobrenome 
 
 console.log(fullName())
 console.log(fullName('Mauro','Martins'))
