2ef0c02fc087d36f103ce252a3aa7b8855439db8
Mlpm2004 <94148203+Mlpm2004@users.noreply.github.com>
2021-11-24 09:05:04 -0300

    Exercício 2

--- Sem6Listaex2.js
+++ Sem6Listaex2.js
@@ -1,3 +1,3 @@
 const frutas = ['banana','manga','maçã','limão','morango']
-//frutas.forEach(function(item){console.log(item)}) 
+//frutas.forEach(function(item){console.log(item)}) Essa linha foi fatorada
 frutas.forEach((item)=>console.log(item)) 
