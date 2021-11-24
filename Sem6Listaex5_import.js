8dc77159bc748392c806c12c2e7b7acda2f407dd
Mlpm2004 <94148203+Mlpm2004@users.noreply.github.com>
2021-11-24 09:09:11 -0300

    Exercício 5 - arquivo com o import

--- Sem6Listaex5_import.js
+++ Sem6Listaex5_import.js
@@ -1,3 +1,3 @@
 //const somar = (numero1, numero2)=> numero1+numero2
 //const dividir = (numero1, numero2)=> numero1/numero2
-import {somar,dividir} from "http://localhost/DEVinHouse/Sem6Listaex5.js"
+import {somar,dividir} from "http://localhost/DEVinHouse/Sem6Listaex5.js" 
