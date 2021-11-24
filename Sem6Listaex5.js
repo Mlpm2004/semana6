1f3a22ae0634d94ddf5c2c2bdf8a9945f6ec14a6
Mlpm2004 <94148203+Mlpm2004@users.noreply.github.com>
2021-11-24 09:08:04 -0300

    Update Sem6Listaex5.js

--- Sem6Listaex5.js
+++ Sem6Listaex5.js
@@ -1,5 +1,5 @@
 export const somar = (numero1, numero2)=> numero1+numero2
-export const dividir = (numero1, numero2)=> numero1/numero2
+export const dividir = (numero1, numero2)=> numero1/numero2 
 
 
 
