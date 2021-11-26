cde0e93873a8193948bca997fc49258ecd85728e
unknown <mpmartins@bol.com.br>
2021-11-26 17:03:57 -0300

    Ex. feito em aula TIMER -usar o mp3 - tem som agora

--- Sem6aula4.js
+++ Sem6aula4.js
@@ -3,7 +3,7 @@ let tempo=0
 let temporizador=0
 for (let i=0; i<botao.length;i++) botao[i].addEventListener('click',()=>setValor(botao[i].name))
 btinicio.addEventListener('click',()=>startTemp())
-btpara.addEventListener('click',()=>stopTemp()) 
+btpara.addEventListener('click',()=>stopTemp("1")) 
 function setValor(tmp){
     tempo=tmp
     let minuto=tempo/60
@@ -16,9 +16,10 @@ function startTemp(){
         let minuto=tempo/60-1
         let segundo=60
         temporizador = setInterval(() => {
-            (minuto==0 && segundo==0)?stopTemp():{}
             segundo--
-            if (segundo==0){
+            (minuto==0 && segundo==0)?stopTemp("0"):{}
+        
+            if (segundo==0 && minuto!=0){
                 minuto--
                 segundo=59
             }
@@ -26,8 +27,13 @@ function startTemp(){
         }, 1000);
     }
 }
-function stopTemp(){
+function stopTemp(tipo){
     clearInterval(temporizador)
+    if (tipo==0){
+        let audio = new Audio("./audio_hero_s-horse-whinny.mp3");
+        audio.play();
+    }
+    
 }
 function formataNumero(numero){
     numero<=9?numero="0"+numero.toString():numero.toString()
